73ffa3e74eddca7816b0d7fd8cf620e14e6fb967
elenamurgiaam <elena.murgia@amdigital.co.uk>
2025-10-10 15:56:02 +0100

    End of Section 7

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,52 +1,15 @@
-import { Box, Container, CssBaseline, Typography} from "@mui/material";
-import { useState } from "react";
+import { Box, Container, CssBaseline } from "@mui/material";
 import Navbar from "./Navbar";
-import ActivityDasboard from "../../features/activities/dashboard/ActivityDasboard";
-import { useActivities } from "../../lib/hooks/UseActivities";
+import { Outlet } from "react-router";
 
 function App() {
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined)
-  const [editMode, setEditMode] = useState(false);
-
-  const {activities, isPending} = useActivities();
-
-  const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities!.find(x => x.id === id));
-  }
-
-  const handleCancelSelectActivity = () => {
-    setSelectedActivity(undefined);
-  }
-
-  const handleOpenForm = (id?: string) => {
-    if (id) handleSelectActivity(id);
-    else handleCancelSelectActivity();
-    setEditMode(true);
-  }
-
-  const handleFormClose = () => {
-    setEditMode(false);
-  }
 
   return (
     <Box sx={{bgcolor: '#eeeeee', minHeight: '100vh'}}>
     <CssBaseline/> {/* to make the navbar take the entire width */}
-      <Navbar openForm={handleOpenForm}/>
+      <Navbar />
       <Container maxWidth='xl' sx={{mt: 3}}>
-        {!activities || isPending ? (
-          <Typography>Loading...</Typography>
-        ) : (
-          <ActivityDasboard 
-          activities={activities}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          selectedActivity={selectedActivity}
-          editMode={editMode}
-          openForm={handleOpenForm}
-          closeForm={handleFormClose}
-
-        />
-        )}
+        <Outlet />
       </Container>
      </Box>
   )
