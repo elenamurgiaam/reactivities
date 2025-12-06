602da02a29227863ddb336efe78f697071a408a0
elenamurgiaam <elena.murgia@amdigital.co.uk>
2025-12-06 23:52:16 +0000

    Section 10

--- client/src/app/layout/Navbar.tsx
+++ client/src/app/layout/Navbar.tsx
@@ -32,6 +32,9 @@ export default function Navbar() {
               <MenuItemLink to='/counter'>
                 Counter
               </MenuItemLink>
+                <MenuItemLink to='/errors'>
+                    Errors
+                </MenuItemLink>
             </Box>
            <MenuItem>
             User menu
